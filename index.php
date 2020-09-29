0951e4e43c5c58cb1df31470ca030d4642fadd60
Sizwe-Shadow <mzilasizwe@gmail.com>
2020-09-29 11:32:40 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width" />
+    <title>index</title>
+    <link rel="stylesheet" href="css/index.css">
+
+</head>
+<body>
+<div class="container" >
+    <div class="app">
+        <img src="img/witlogo.png" width="auto" height="auto" class="center">
+        <h2 class="page-heading"><span>Welcome to the Wits App</span></h2>
+        <a href="Login.php">
+            <button class="login-button" type="login-button">Login</button>
+        </a>
+        <a href="register.html">
+            <button class="signup-button"type="signup-button">Sign Up</button>
+        </a>
+
+
+    </div>
+
+</div>
+</body>
+</html>
